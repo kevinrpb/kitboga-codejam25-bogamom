cbe81d94de21657974fdf2c7509914160e2a9823
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2025-06-17 19:33:23 -0400

    Add character and an animated 'bogaball'
    The capturing animation still needs some work.

--- captcha/captcha.js
+++ captcha/captcha.js
@@ -3,15 +3,21 @@ console.log("Hello world");
 const State = {
 	Start: "state-start",
 	ShowMoves: "state-show-moves",
+	ThrowBall: "state-throw-ball",
 };
 
 const States = {
 	Start: () => ({ name: State.Start }),
 	ShowMoves: () => ({ name: State.ShowMoves }),
+	ThrowBall: () => ({ name: State.ThrowBall }),
 };
 
 let currentState = States.Start();
 
+const bogaball = document.getElementById("bogaball");
+const bitcoinImage = document.getElementById("bitcoin-image");
+
+const ballButton = document.getElementById("ball-button");
 const fightButton = document.getElementById("fight-button");
 const movesContainer = document.getElementById("moves-container");
 
@@ -20,9 +26,21 @@ fightButton.addEventListener("click", () => {
 	currentState = States.ShowMoves();
 });
 
+ballButton.addEventListener("click", () => {
+	bogaball.classList.add("throw");
+	bitcoinImage.classList.add("throw");
+	currentState = States.ThrowBall();
+});
+
 document.addEventListener("keydown", (event) => {
-	if (event.key === "Escape" && currentState.name === State.ShowMoves) {
-		movesContainer.classList.add("hide");
-		currentState = States.Start();
+	if (event.key === "Escape") {
+		if (currentState.name === State.ShowMoves) {
+			movesContainer.classList.add("hide");
+			currentState = States.Start();
+		} else if (currentState.name === State.ThrowBall) {
+			bogaball.classList.remove("throw");
+			bitcoinImage.classList.remove("throw");
+			currentState = States.Start();
+		}
 	}
 });
