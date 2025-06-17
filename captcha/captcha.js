e08b13a34f981b9285d7c9a6f934b35a4dd6cce7
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2025-06-17 12:46:07 -0400

    Set up initial menus and part of the characters

--- captcha/captcha.js
+++ captcha/captcha.js
@@ -1,1 +1,28 @@
 console.log("Hello world");
+
+const State = {
+	Start: "state-start",
+	ShowMoves: "state-show-moves",
+};
+
+const States = {
+	Start: () => ({ name: State.Start }),
+	ShowMoves: () => ({ name: State.ShowMoves }),
+};
+
+let currentState = States.Start();
+
+const fightButton = document.getElementById("fight-button");
+const movesContainer = document.getElementById("moves-container");
+
+fightButton.addEventListener("click", () => {
+	movesContainer.classList.remove("hide");
+	currentState = States.ShowMoves();
+});
+
+document.addEventListener("keydown", (event) => {
+	if (event.key === "Escape" && currentState.name === State.ShowMoves) {
+		movesContainer.classList.add("hide");
+		currentState = States.Start();
+	}
+});
