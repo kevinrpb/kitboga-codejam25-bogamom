86fe72de2008ac42b619b649b3386a2be966ab3f
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2025-06-18 23:11:09 -0400

    Refactor state handling to use events

--- captcha/captcha.js
+++ captcha/captcha.js
@@ -1,9 +1,28 @@
-console.log("Hello world");
+/**
+ ** State definitions
+ */
 
 const State = {
 	Start: "state-start",
 	ShowMoves: "state-show-moves",
 	ThrowBall: "state-throw-ball",
+	/**
+	 *
+	 * @param {string} state
+	 * @param {EventListenerOrEventListenerObject} handler
+	 */
+	on: (state, handler) => {
+		document.addEventListener(state, handler);
+	},
+	/**
+	 *
+	 * @param {{ name: string, data?: object }} state
+	 */
+	set: (state) => {
+		currentState = state;
+		const event = new CustomEvent(state.name, { detail: state });
+		document.dispatchEvent(event);
+	},
 };
 
 const States = {
@@ -11,6 +30,10 @@ const States = {
 	ShowMoves: () => ({ name: State.ShowMoves }),
 	ThrowBall: () => ({ name: State.ThrowBall }),
 };
+
+/**
+ ** Variables & Elements
+ */
 
 let currentState = States.Start();
 
@@ -21,26 +44,36 @@ const ballButton = document.getElementById("ball-button");
 const fightButton = document.getElementById("fight-button");
 const movesContainer = document.getElementById("moves-container");
 
-fightButton.addEventListener("click", () => {
-	movesContainer.classList.remove("hide");
-	currentState = States.ShowMoves();
+/**
+ ** State transitions
+ */
+
+State.on(State.Start, () => {
+	movesContainer.classList.add("hide");
 });
 
-ballButton.addEventListener("click", () => {
+State.on(State.ShowMoves, () => {
+	movesContainer.classList.remove("hide");
+});
+
+State.on(State.ThrowBall, () => {
 	bogaball.classList.add("throw");
 	bitcoinImage.classList.add("throw");
-	currentState = States.ThrowBall();
 });
+
+/**
+ ** UI Handling
+ */
+
+fightButton.addEventListener("click", () => State.set(States.ShowMoves()));
+ballButton.addEventListener("click", () => State.set(States.ThrowBall()));
 
 document.addEventListener("keydown", (event) => {
 	if (event.key === "Escape") {
 		if (currentState.name === State.ShowMoves) {
-			movesContainer.classList.add("hide");
-			currentState = States.Start();
+			State.set(States.Start());
 		} else if (currentState.name === State.ThrowBall) {
-			bogaball.classList.remove("throw");
-			bitcoinImage.classList.remove("throw");
-			currentState = States.Start();
+			State.set(States.Start());
 		}
 	}
 });
